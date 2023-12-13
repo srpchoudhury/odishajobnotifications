589097842e0ad06865acd4aa911647ba86dbc68f
S Rudra Prasad Choudhury <98145990+srpchoudhury@users.noreply.github.com>
2023-12-13 19:00:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,3 +21,80 @@ document.addEventListener('DOMContentLoaded', function () {
         event.stopPropagation();
     });
 });
+
+
+
+
+ 
+// Function to get card data from the static cards on the page
+function getCardData() {
+    const cardsData = [];
+    const cardElements = document.querySelectorAll('.card');
+
+    cardElements.forEach(function (cardElement) {
+        const title = cardElement.querySelector('h4').textContent;
+        const description = cardElement.querySelector('p').textContent;
+        const link = cardElement.querySelector('.badge.bg-info a').getAttribute('href');
+        cardsData.push({ title, description, link });
+    });
+
+    return cardsData;
+}
+
+// Function to display cards based on search input
+function displayCards(data) {
+    const cardContainer = document.getElementById('cardContainer');
+    cardContainer.innerHTML = ''; // Clear existing cards
+
+    // Filter cards based on search input
+    const searchInput = document.getElementById('searchInput').value.toLowerCase();
+    
+    if (searchInput.trim() === "") {
+        // If search input is empty, display all cards
+        data.forEach(function (card) {
+            const cardHtml = `
+                <div class="col-md-4">
+                    <div class="card p-2">
+                        <div class="card-body">
+                            <h4 style="color: blue;">${card.title}</h4>
+                            <p style="color: green;">${card.description}</p>
+                            <h5>
+                                <span class="badge bg-dark">New ------- ></span>
+                                <span class="badge bg-info"><a class="nav-link" href="${card.link}">View Details</a></span>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            `;
+            cardContainer.insertAdjacentHTML('beforeend', cardHtml);
+        });
+    } else {
+        // If search input is not empty, filter and display matching cards
+        const filteredData = data.filter(function (card) {
+            return card.title.toLowerCase().includes(searchInput) || card.description.toLowerCase().includes(searchInput);
+        });
+
+        filteredData.forEach(function (card) {
+            const cardHtml = `
+                <div class="col-md-4">
+                    <div class="card p-2">
+                        <div class="card-body">
+                            <h4 style="color: blue;">${card.title}</h4>
+                            <p style="color: green;">${card.description}</p>
+                            <h5>
+                                <span class="badge bg-dark">New ------- ></span>
+                                <span class="badge bg-info"><a class="nav-link" href="${card.link}">View Details</a></span>
+                            </h5>
+                        </div>
+                    </div>
+                </div>
+            `;
+            cardContainer.insertAdjacentHTML('beforeend', cardHtml);
+        });
+    }
+}
+
+// Event listener for search input
+document.getElementById('searchInput').addEventListener('input', function () {
+    displayCards(getCardData());
+});
