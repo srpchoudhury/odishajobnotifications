27aef7082d134ee742e46ffa301e2e409b327105
S Rudra Prasad Choudhury <98145990+srpchoudhury@users.noreply.github.com>
2024-01-31 19:46:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -138,16 +138,3 @@ document.getElementById('searchInput').addEventListener('input', function () {
         });
     });
 
-//for current date of post
-var detailLinks = document.querySelectorAll('.detailsLink');
- detailLinks.forEach(function(link) {
-    
-     var currentDate = new Date();
-   
-     var formattedDate = currentDate.getDate().toString().padStart(2, '0') + '-' + (currentDate.getMonth() + 1).toString().padStart(2, '0') + '-' + currentDate.getFullYear();
-    
-     var currentDateElement = link.querySelector('.currentDate');
-     if (currentDateElement) {
-         currentDateElement.innerText =  "(Uploaded On) " +formattedDate;
-     }
-    });
